7a4a6e6174b576b7590404dc6d9673d0cbc5d01e
sinanozlen <sinanozlen1@gmail.com>
2024-07-24 21:45:34 +0300

    about eklendi

---
+++ Frontends/WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _NavbarUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+
+        }
+    }
+}

6b561a1530070667f847f8b32154cec88302ae37
furkangul29 <gulfurkan@outlook.com.tr>
2024-07-26 16:26:14 +0300

    Tema icin gerekli controllerlar olusturuldu. Tema Tamamıyla ViewComponentlere baglı statik olarak calısır hale getirildi.

---
+++ Frontends/WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v0 = "Kategoriler";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Sayfalar";
+            ViewBag.v3 = "Kategoriler";
+            return View();
+        }
+    }
+}
